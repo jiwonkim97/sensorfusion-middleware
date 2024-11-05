7ed1fe06ec7f464027c5ac8ce4342327335b12ac
jiwonkim97 <kimjiwon97@gmail.com>
2024-11-05 07:37:19 +0000

    feat: several sensor data api

---
+++ app/api/outputs/camera1/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server';
+import outputData from '@/mocks/output.json';
+
+const TIME_RANGE = 1000 * 60 * 5;
+const TIME_INTERVAL = 1000 * 5;
+const HZ = 30;
+const DATA_COUNT = (TIME_RANGE * HZ) / TIME_INTERVAL;
+
+let cnt = 0;
+const camera1Outputs = outputData.filter((d) => d.sensor_name === 'Camera1');
+const camera2Outputs = outputData.filter((d) => d.sensor_name === 'Camera2');
+const camera3Outputs = outputData.filter((d) => d.sensor_name === 'Camera3');
+const camera4Outputs = outputData.filter((d) => d.sensor_name === 'Camera4');
+const lidar1Outputs = outputData.filter((d) => d.sensor_name === 'LiDAR1');
+const maxCnt = Math.floor(
+  Math.min(
+    camera1Outputs.length,
+    camera2Outputs.length,
+    camera3Outputs.length,
+    camera4Outputs.length,
+    lidar1Outputs.length
+  )
+);
+
+export const GET = async () => {
+  const endCnt = cnt + DATA_COUNT;
+  if (endCnt > maxCnt) {
+    cnt = 0;
+  }
+  const response = NextResponse.json(refreshTimestamp(camera1Outputs.slice(cnt, endCnt)));
+  cnt = TIME_INTERVAL * HZ;
+
+  return response;
+};
+
+const refreshTimestamp = (data: typeof camera1Outputs) => {
+  const now = new Date();
+
+  return [...data].map((d, idx) => ({
+    ...d,
+    timestamp: new Date(now.getTime() - idx * 1000 + 1000 / HZ).toISOString(),
+  }));
+};
