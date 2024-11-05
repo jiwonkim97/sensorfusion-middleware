e5e826f0ca5ef2a3e266fc836127dc13729da112
jiwonkim97 <kimjiwon97@gmail.com>
2024-11-05 09:02:29 +0000

    fix: cache type int to object

--- app/api/outputs/camera1/route.ts
+++ app/api/outputs/camera1/route.ts
@@ -5,22 +5,23 @@ const TIME_RANGE = 1000 * 60 * 5;
 const TIME_INTERVAL = 1000 * 1;
 const DATA_COUNT = TIME_RANGE / TIME_INTERVAL;
 
-let cnt = 0;
+const cache = { camera1Cnt: 1 };
+
 const camera1Outputs = outputData.filter((d) => d.sensor_name === 'Camera1');
 const data = [...camera1Outputs, ...camera1Outputs, ...camera1Outputs, ...camera1Outputs];
 const maxCnt = Math.floor(data.length);
 
 export const GET = async () => {
-  console.log('cnt: ', cnt, new Date());
-  const endCnt = cnt + DATA_COUNT;
+  console.log('cnt: ', cache.camera1Cnt, new Date());
+  const endCnt = cache.camera1Cnt + DATA_COUNT;
   console.log('endCnt: ', endCnt);
   if (endCnt > maxCnt) {
     console.log('overflow!', endCnt, maxCnt);
-    cnt = 0;
+    cache.camera1Cnt = 0;
   }
-  const response = NextResponse.json(refreshTimestamp(data.slice(cnt, endCnt)));
-  cnt += 1;
-  console.log('after cnt: ', cnt);
+  const response = NextResponse.json(refreshTimestamp(data.slice(cache.camera1Cnt, endCnt)));
+  cache.camera1Cnt = cache.camera1Cnt + 1;
+  console.log('after cnt: ', cache.camera1Cnt);
 
   return response;
 };
