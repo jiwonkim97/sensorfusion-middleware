fc8a08b0ffd4f60282bfff2adeb271176acc8fc7
jiwonkim97 <kimjiwon97@gmail.com>
2024-11-05 04:50:59 +0000

    fix: remove unused data

--- app/api/nodes/route.ts
+++ app/api/nodes/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-import nodeData from '@/mocks/node.json'
+// import nodeData from '@/mocks/node.json'
 
 export const GET = () => {
   return NextResponse.json([{
