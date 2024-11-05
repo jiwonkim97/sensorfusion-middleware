3059cf3c5f1a0589446df050bb536cca555a9bfc
jiwonkim97 <kimjiwon97@gmail.com>
2024-11-05 06:17:34 +0000

    fix: error

--- app/api/outputs/[sensor_name]/route.ts
+++ app/api/outputs/[sensor_name]/route.ts
@@ -1,6 +1,5 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-let
 export const GET = (request: NextRequest, { params }: { params: { sensor_name: string; } }) => {
-
+  return NextResponse.json({result: 'success'})
 }
