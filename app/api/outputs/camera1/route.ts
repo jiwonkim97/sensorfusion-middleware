839653a4079c03f82393cd95d501b34e9796130a
jiwonkim97 <kimjiwon97@gmail.com>
2024-11-05 07:54:47 +0000

    fix: remove HZ

--- app/api/outputs/camera1/route.ts
+++ app/api/outputs/camera1/route.ts
@@ -2,22 +2,23 @@ import { NextResponse } from 'next/server';
 import outputData from '@/mocks/output.json';
 
 const TIME_RANGE = 1000 * 60 * 5;
-const TIME_INTERVAL = 1000 * 5;
-const HZ = 30;
-const DATA_COUNT = (TIME_RANGE * HZ) / TIME_INTERVAL;
+const TIME_INTERVAL = 1000 * 1;
+const DATA_COUNT = (TIME_RANGE ) / TIME_INTERVAL;
 
 let cnt = 0;
 const camera1Outputs = outputData.filter((d) => d.sensor_name === 'Camera1');
-const maxCnt = Math.floor(camera1Outputs.length);
+const data = [...camera1Outputs, ...camera1Outputs, ...camera1Outputs, ...camera1Outputs]
+const maxCnt = Math.floor(data.length);
 
 export const GET = async () => {
   console.log('cnt: ', cnt);
   const endCnt = cnt + DATA_COUNT;
   console.log('endCnt: ', endCnt);
   if (endCnt > maxCnt) {
+    console.log('overflow!', endCnt, maxCnt);
     cnt = 0;
   }
-  const response = NextResponse.json(refreshTimestamp(camera1Outputs.slice(cnt, endCnt)));
+  const response = NextResponse.json(refreshTimestamp(data.slice(cnt, endCnt)));
   cnt += 1;
   console.log('after cnt: ', cnt);
 
@@ -29,6 +30,6 @@ const refreshTimestamp = (data: typeof camera1Outputs) => {
 
   return [...data].map((d, idx) => ({
     ...d,
-    timestamp: new Date(now.getTime() - idx * 1000 + 1000 / HZ).toISOString(),
+    timestamp: new Date(now.getTime() - idx * 1000).toISOString(),
   }));
 };
