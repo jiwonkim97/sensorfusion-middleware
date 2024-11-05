f4de41fc28238b0cc377d935a9eee0465f179f9c
jiwonkim97 <kimjiwon97@gmail.com>
2024-11-05 08:14:53 +0000

    feat: add other sensor api and outputs cnt add logic

--- app/api/outputs/route.ts
+++ app/api/outputs/route.ts
@@ -1,18 +1,26 @@
-import { NextResponse } from "next/server"
-import outputData from '@/mocks/output.json'
+import { NextResponse } from 'next/server';
+import outputData from '@/mocks/output.json';
 
 let cnt = 0;
-const  camera1Data = outputData.filter(d => d.sensor_name === 'Camera1')
-const  camera2Data = outputData.filter(d => d.sensor_name === 'Camera2')
-const  camera3Data = outputData.filter(d => d.sensor_name === 'Camera3')
-const  camera4Data = outputData.filter(d => d.sensor_name === 'Camera4')
-const  lidar1Data = outputData.filter(d => d.sensor_name === 'LiDAR1')
-const maxCnt = Math.floor(Math.min(camera1Data.length, camera2Data.length, camera3Data.length, camera4Data.length, lidar1Data.length))
+const camera1Data = outputData.filter((d) => d.sensor_name === 'Camera1');
+const camera2Data = outputData.filter((d) => d.sensor_name === 'Camera2');
+const camera3Data = outputData.filter((d) => d.sensor_name === 'Camera3');
+const camera4Data = outputData.filter((d) => d.sensor_name === 'Camera4');
+const lidar1Data = outputData.filter((d) => d.sensor_name === 'LiDAR1');
+const maxCnt = Math.floor(
+  Math.min(camera1Data.length, camera2Data.length, camera3Data.length, camera4Data.length, lidar1Data.length)
+);
 
 export const GET = () => {
-  const response = NextResponse.json([camera1Data[cnt], camera2Data[cnt], camera3Data[cnt], camera4Data[cnt], lidar1Data[cnt]])
-  cnt += 30;
+  const response = NextResponse.json([
+    camera1Data[cnt],
+    camera2Data[cnt],
+    camera3Data[cnt],
+    camera4Data[cnt],
+    lidar1Data[cnt],
+  ]);
+  cnt += 1;
   cnt %= maxCnt;
 
-  return response
-}
+  return response;
+};
