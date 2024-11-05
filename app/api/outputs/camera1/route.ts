6a4857402bda9384e81963fb08895cf5fb7267c3
jiwonkim97 <kimjiwon97@gmail.com>
2024-11-05 08:51:42 +0000

    add timestamp log

--- app/api/outputs/camera1/route.ts
+++ app/api/outputs/camera1/route.ts
@@ -3,15 +3,15 @@ import outputData from '@/mocks/output.json';
 
 const TIME_RANGE = 1000 * 60 * 5;
 const TIME_INTERVAL = 1000 * 1;
-const DATA_COUNT = (TIME_RANGE ) / TIME_INTERVAL;
+const DATA_COUNT = TIME_RANGE / TIME_INTERVAL;
 
 let cnt = 0;
 const camera1Outputs = outputData.filter((d) => d.sensor_name === 'Camera1');
-const data = [...camera1Outputs, ...camera1Outputs, ...camera1Outputs, ...camera1Outputs]
+const data = [...camera1Outputs, ...camera1Outputs, ...camera1Outputs, ...camera1Outputs];
 const maxCnt = Math.floor(data.length);
 
 export const GET = async () => {
-  console.log('cnt: ', cnt);
+  console.log('cnt: ', cnt, new Date());
   const endCnt = cnt + DATA_COUNT;
   console.log('endCnt: ', endCnt);
   if (endCnt > maxCnt) {
