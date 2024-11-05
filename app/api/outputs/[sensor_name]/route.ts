c6dfb53a173a6fb0df874388f6ee480029e4dfdc
jiwonkim97 <kimjiwon97@gmail.com>
2024-11-05 07:20:45 +0000

    fix: timestream data hz

--- app/api/outputs/[sensor_name]/route.ts
+++ app/api/outputs/[sensor_name]/route.ts
@@ -3,7 +3,8 @@ import outputData from '@/mocks/output.json';
 
 const TIME_RANGE = 1000 * 60 * 5;
 const TIME_INTERVAL = 1000 * 5;
-const DATA_COUNT = TIME_RANGE / TIME_INTERVAL;
+const HZ = 30;
+const DATA_COUNT = (TIME_RANGE * HZ) / TIME_INTERVAL;
 
 let cnt = 0;
 const camera1Outputs = outputData.filter((d) => d.sensor_name === 'Camera1');
@@ -52,6 +53,6 @@ const refreshTimestamp = (data: typeof camera1Outputs) => {
 
   return [...data].map((d, idx) => ({
     ...d,
-    timestamp: new Date(now.getTime() - idx * 1000).toISOString(),
+    timestamp: new Date(now.getTime() - idx * 1000 + 1000 / HZ).toISOString(),
   }));
 };
